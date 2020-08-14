b1da65bc2967f13bee45e2b0744b55929df229e8
Jan Rudolf <jan.rudolf@plus4u.net>
2020-08-14 09:55:40 +0200

    Added metadata and SEO optimalizations.

--- uu_strava_maing01-hi/src/config/backlog-config.js
+++ uu_strava_maing01-hi/src/config/backlog-config.js
@@ -4,9 +4,6 @@ const BacklogConfig = {
       cs: "Historie výsledků - celkový přehled, jednotlivec, etapa atd."
     },
     {
-      cs: "Favicona, meta data, podpora pro vyhledávače, noscript varování"
-    },
-    {
       cs: "Appka do Google a iPhone storu"
     },
     {
