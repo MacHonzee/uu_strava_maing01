6d0fedcdfba1f93da4cf98870d4cb59f29d227e2
Jan Rudolf <jan.rudolf@plus4u.net>
2022-05-02 10:46:08 +0200

    Reworked GoogleApiHelper.calculateElevation, because apparently it broke for enconding with more than 512 coordinates, which were majority of 20km+ trails. It now passes coordinate list instead of encoded string, and it merges the requests together by 250 coord lists.

--- uu_strava_maing01-server/app/components/google-api-helper.js
+++ uu_strava_maing01-server/app/components/google-api-helper.js
@@ -4,7 +4,7 @@ const polyline = require("@mapbox/polyline");
 const Errors = require("../api/errors/google-api-error");
 
 const ELEVATION_API_URL = "https://maps.googleapis.com/maps/api/elevation/json";
-const MAX_SAMPLES = 500;
+const MAX_SAMPLES = 250;
 
 const GoogleApiHelper = {
   async calculateElevation(segment, apiKey) {
@@ -12,15 +12,23 @@ const GoogleApiHelper = {
     let path = segment.map.polyline;
     let coords = polyline.decode(path);
 
-    // call google Api for the result
-    let samples = Math.min(MAX_SAMPLES, coords.length);
-    let dtoIn = { key: apiKey, path: "enc:" + path, samples };
-    let dtoOut = await AppClient.get(ELEVATION_API_URL, dtoIn);
-    if (dtoOut.data.status !== "OK") {
-      throw new Errors.CalculateElevation.InvalidGoogleApiResult({}, { segment, response: dtoOut.data });
-    } else {
-      return dtoOut.data.results;
+    // slice it by MAX_SAMPLES (there is a limit to Google API) and merge the results together
+    let allResults = [];
+    for (let i = 0; i <= Math.floor(coords.length / MAX_SAMPLES); i++) {
+      // build correct dtoIn
+      let slicedCoords = coords.slice(i * MAX_SAMPLES, (i + 1) * MAX_SAMPLES);
+      let samples = Math.min(MAX_SAMPLES, slicedCoords.length);
+      let dtoIn = { key: apiKey, path: slicedCoords.join("|"), samples };
+
+      let dtoOut = await AppClient.get(ELEVATION_API_URL, dtoIn);
+      if (dtoOut.data.status !== "OK") {
+        throw new Errors.CalculateElevation.InvalidGoogleApiResult({}, { segment, response: dtoOut.data });
+      } else {
+        allResults = allResults.concat(dtoOut.data.results);
+      }
     }
+
+    return allResults;
   },
 };
 
